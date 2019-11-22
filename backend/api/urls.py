af0e7f4364e85d6a6eaa3c2b4035f53f1e2d959d
hwangyeojin <hwangyj0202@gmail.com>
2019-11-23 01:41:07 +0900

    checklist func 구현

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,5 +5,5 @@ from api.views import pre_travel_views
 urlpatterns = [
     url('auth/signup/$', auth_views.signup, name='sign_up'),
     url('users/$', auth_views.users, name='users'),
-    url('get_balance/', pre_travel_views.get_balance, name='get_balance'),
+    url('get_balance/$', pre_travel_views.get_balance, name='get_balance'),
 ]

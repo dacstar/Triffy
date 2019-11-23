087e2dae49ab6d52d01de8f46edb783e7c1affa2
hwangyeojin <hwangyj0202@gmail.com>
2019-11-23 16:34:09 +0900

    debugging

--- backend/api/views/inter_travel_views.py
+++ backend/api/views/inter_travel_views.py
@@ -1,3 +1,5 @@
+import requests
+import json
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -13,6 +15,62 @@ def get_calendar(request):
     if request.method == 'POST':
         if request.user.is_authenticated:
             user = request.user
+            # 신용카드 해외 사용 내역 조회
+            url = "http://10.3.17.61:8081/v1/usecreditcard/searchuseforoverseas"
+            params = {
+                "dataHeader":{
+                },
+                "dataBody":{
+                    "nxtQyKey":"",
+                    "fromdate":"20190506",
+                    "todate":"20190806",
+                    "bctag":"0",
+                    "fmlOCrdC":"0"
+                }
+            }
+            response = requests.post(url, data=json.dumps(params)).json()
+            print(response)
+            if response["dataBody"]["grp001"]:
+                group = response["dataBody"]["grp001"]
+                for item in group:
+                    content = item["ryCd"]
+                    if content == "택시":
+                        category = "교통비"
+                    money = float(item["aprvamt"].replace(',', ''))
+                    time_now = item["aprvtime"][:8]
+                    spend = True
+                    currency = item["currency"]
+                    card = True
+                    if not Calendar.objects.filter(user_id=user, time_now=timenow, content=content, money=money):
+                        result = Calendar.objects.create(user_id=user, category=category, time_now=time_now, content=content, money=money, card=card, spent=spent, currency=currency)
+
+            # 체크카드 해외 사용내역 조회
+            url = "http://10.3.17.61:8081/v1/usedebitcard/searchuseforoverseas"
+            params = {
+                "dataHeader":{
+                },
+                "dataBody":{
+                    "nxtQyKey":"",
+                    "fromdate":"20190507",
+                    "todate":"20190830",
+                    "bctag":"0"
+                }
+            }
+            response = requests.post(url, data=json.dumps(params)).json()
+            print(response)
+            if response["dataBody"]["grp001"]:
+                group = response["dataBody"]["grp001"]
+                for item in group:
+                    content = item["ryCd"]
+                    if content == "주차장":
+                        category = "기타"
+                    currency = item["loaTel"]
+                    time_now = item["apvDt"][:8]
+                    money = float(item["apva"])
+                    card = True
+                    spent = True
+                    if not Calendar.objects.filter(user_id=user, time_now=timenow, content=content, money=money):
+                        result = Calendar.objects.create(user_id=user, category=category, time_now=time_now, content=content, money=money, card=card, spent=spent, currency=currency)
             calendars = Calendar.objects.filter(user_id=user)
             result = {}
             for item in calendars:
@@ -68,6 +126,65 @@ def by_card(request):
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
+# @api_view(['POST'])
+# def buy_card(request):
+#     if request.method == 'POST':
+#         user = request.user
+#         # 신용카드 해외 사용 내역 조회
+#         url = 'http://10.3.17.61:8081/v1/usecreditcard/searchuseforoverseas'
+#         params = {
+#             "dataHeader":{
+#             },
+#             "dataBody":{
+#                 "nxtQyKey":"",
+#                 "fromdate":"20190506",
+#                 "todate":"20190806",
+#         "bctag":"0",
+#         "fmlOCrdC":"0"
+#             }
+#         }
+#         response = requests.post(url, data=json.dumps(params)).json()
+#         if response['dataBody']['grp001']:
+#             group = response['dataBody']['grp001']
+#             for item in group:
+#                 content = item['ryCd']
+#                 if content == '택시':
+#                     category = '교통비'
+#                 money = int(item['aprvamt'].replace(',', ''))
+#                 time_now = item['aprvtime'][:8]
+#                 spend = True
+#                 currency = item['currency']
+#                 card = True
+#                 if not Calendar.objects.filter(user_id=user, time_now=timenow, content=content, money=money):
+#                     result = Calendar.objects.create(user_id=user, category=category, time_now=time_now, content=content, money=money, card=card, spent=spent, currency=currency)
+
+#         # 체크카드 해외 사용내역 조회
+#         url = 'http://10.3.17.61:8081/v1/usedebitcard/searchuseforoverseas'
+#         params = {
+#             "dataHeader":{
+#             },
+#             "dataBody":{
+#                 "nxtQyKey":"",
+#         "fromdate":"20190507",
+#         "todate":"20190830",
+#         "bctag":"0"
+#             }
+#         }
+#         response = requests.post(url, data=json.dumps(params)).json()
+#         if response['dataBody']['grp001']:
+#             group = response['dataBody']['grp001']
+#             for item in group:
+#                 content = item['ryCd']
+#                 if content == '주차장':
+#                     category = '기타'
+#                 currency = item['loaTel']
+#                 time_now = item['apvDt'][:8]
+#                 money = float(item['apva'])
+#                 card = True
+#                 spent = True
+#                 if not Calendar.objects.filter(user_id=user, time_now=timenow, content=content, money=money):
+#                     result = Calendar.objects.create(user_id=user, category=category, time_now=time_now, content=content, money=money, card=card, spent=spent, currency=currency)
+        
 
 # calendar item 생성하기(spent가 true면 이미 소비한 내역, false면 소비계획)
 @api_view(['POST'])
@@ -78,8 +195,8 @@ def add_item(request):
             category = request.POST.get('category', None)
             time_now = str(datetime.now())[:10]
             content = request.POST['content']
-            money = int(request.POST['money'])
-            card = request.POST.get('card', False)
+            money = float(request.POST['money'])
+            # card = request.POST.get('card', False)
             spent = request.POST.get('spent', True)
             currency = request.POST.get('currency', 'won')
             calendar = Calendar.objects.create(user_id=user, category=category, time_now=time_now, content=content, money=money, card=card, spent=spent, currency=currency)
@@ -91,7 +208,7 @@ def add_item(request):
             return Response(data=result, status=status.HTTP_200_OK)
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
-        
+
 
 @api_view(['POST'])
 def switch_spent(request):
@@ -151,7 +268,7 @@ def edit_item(request):
             category = request.POST.get('category', None)
             content = request.POST.get('content', None)
             money = request.POST.get('money', None)
-            card = request.POST.get('card', None)
+            # card = request.POST.get('card', None)
             spent = request.POST.get('spent', None)
             currency = request.POST.get('currency', None)
             if category is not None:
@@ -159,7 +276,7 @@ def edit_item(request):
             if content is not None:
                 calendar.content = content
             if money is not None:
-                calendar.money = int(money)
+                calendar.money = float(money)
             if card is not None:
                 calendar.card = card
             if spent is not None:

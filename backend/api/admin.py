2649d18d98d0c92d0992b66cbe0ad77bb49b0928
hwangyeojin <hwangyj0202@gmail.com>
2019-11-24 07:49:44 +0900

    delete

--- backend/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Balance, Calendar, CheckList, Post, City
-
-admin.site.register(Profile)
-admin.site.register(Balance)
-admin.site.register(Calendar)
-admin.site.register(CheckList)
-admin.site.register(Post)
-admin.site.register(City)
